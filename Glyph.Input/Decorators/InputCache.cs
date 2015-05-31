c00cebf917dc3073a814c085b4dd9a142f638c96
ReMinoer <rmorillon@freneticexperience.com>
2015-05-31 21:40:17 +0200

    Add RadialMouseHandler, InputCache decorator & MouseManager singleton

---
+++ Glyph.Input/Decorators/InputCache.cs
@@ -0,0 +1,54 @@
+﻿namespace Glyph.Input.Decorators
+{
+    public class InputCache<TValue> : InputDecorator<TValue, IInputHandler<TValue>>
+    {
+        private readonly Period _resetPeriod = new Period();
+        private TValue _cache;
+
+        public int ResetTimeout
+        {
+            get { return _resetPeriod.Interval; }
+            set { _resetPeriod.Interval = value; }
+        }
+
+        public override bool IsActivated
+        {
+            get { return !_resetPeriod.IsEnd; }
+        }
+
+        public override TValue Value
+        {
+            get { return IsActivated ? _cache : default(TValue); }
+        }
+
+        public InputCache()
+            : this("", null, 0)
+        {
+        }
+
+        public InputCache(string name, IInputHandler<TValue> component, int resetTimeout)
+            : base(name, component)
+        {
+            ResetTimeout = resetTimeout;
+            _resetPeriod.InitFromEnd();
+        }
+
+        public override void Update(InputStates inputStates)
+        {
+            base.Update(inputStates);
+
+            if (!IsActivated && Component.Value.Equals(default(TValue)))
+                return;
+
+            if (!_cache.Equals(Component.Value))
+            {
+                _resetPeriod.Init();
+                _cache = Component.Value;
+            }
+            else
+            {
+                _resetPeriod.Update(ElapsedTime.Instance);
+            }
+        }
+    }
+}
---
+++ Glyph.Input/Handlers/Vectors/RadialMouseHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Glyph.Input.Handlers.Vectors
+{
+    public class RadialMouseHandler : VectorHandler
+    {
+        public float VirtualRadius { get; set; }
+
+        public override InputSource InputSource
+        {
+            get { return InputSource.Mouse; }
+        }
+
+        public RadialMouseHandler()
+            : this("", 0)
+        {
+        }
+
+        public RadialMouseHandler(string name, float virtualRadius, float deadZone = 0, bool inverseX = false,
+            bool inverseY = false, InputActivity inputActivity = InputActivity.Pressed)
+            : base(name, deadZone, inverseX, inverseY, inputActivity)
+        {
+            VirtualRadius = virtualRadius;
+        }
+
+        protected override Vector2 GetState(InputStates inputStates)
+        {
+            return (inputStates.MouseState.Position.ToVector2() - MouseManager.Instance.DefaultPosition.ToVector2())
+                   / (VirtualRadius * (Resolution.Instance.ScaleRatio * Camera.Zoom));
+        }
+    }
+}
