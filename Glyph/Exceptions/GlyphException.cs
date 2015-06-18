e310124e4b8e1f4e903c5215a8320c992279d1ab
ReMinoer <rmorillon@freneticexperience.com>
2015-06-18 21:48:47 +0200

    Add base GlyphException

---
+++ Glyph/Exceptions/GlyphException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Glyph.Exceptions
+{
+    public abstract class GlyphException : Exception
+    {
+        protected GlyphException()
+        {
+        }
+
+        protected GlyphException(string message)
+            : base(message)
+        {
+        }
+
+        protected GlyphException(string message, params object[] args)
+            : base(string.Format(message, args))
+        {
+        }
+    }
+}
