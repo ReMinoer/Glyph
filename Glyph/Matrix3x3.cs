de9d8f9faec76ceaac24b5e6c2cd9a473ddf583d
ReMinoer <rmorillon@freneticexperience.com>
2015-06-25 22:57:51 +0200

    Add Matrix3x3 & Transformation to handle 2D transformations

---
+++ Glyph/Matrix3x3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Glyph
+{
+    public struct Matrix3X3
+    {
+        public float M11, M12, M13;
+        public float M21, M22, M23;
+
+        public Matrix3X3(Vector2 translation, float rotation, float scale)
+        {
+            float cos = (float)Math.Cos(rotation);
+            float sin = (float)Math.Sin(rotation);
+
+            M11 = cos * scale;
+            M12 = sin;
+            M13 = translation.X;
+            M21 = -sin;
+            M22 = cos * scale;
+            M23 = translation.Y;
+        }
+
+        public static Vector2 operator *(Matrix3X3 matrix, Vector2 vector)
+        {
+            return new Vector2
+            {
+                X = vector.X * matrix.M11 + vector.Y * matrix.M12 + matrix.M13,
+                Y = vector.X * matrix.M21 + vector.Y * matrix.M22 + matrix.M23
+            };
+        }
+    }
+}
---
+++ Glyph/Transformation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Glyph
+{
+    public struct Transformation
+    {
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public float Scale { get; set; }
+
+        public Matrix3X3 Matrix { get; private set; }
+
+        public static Transformation Identity
+        {
+            get
+            {
+                return new Transformation {
+                    Position = Vector2.Zero,
+                    Rotation = 0,
+                    Scale = 1f
+                };
+            }
+        }
+
+        public Transformation(Vector2 position, float rotation, float scale)
+            : this()
+        {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+
+            RefreshMatrix();
+        }
+
+        public void SetPosition(Vector2 value)
+        {
+            if (value == Position)
+                return;
+
+            Position = value;
+            RefreshMatrix();
+        }
+
+        public void SetRotation(float value)
+        {
+            float angle = MathHelper.WrapAngle(value);
+
+            if (Math.Abs(angle - Rotation) < float.Epsilon)
+                return;
+
+            Rotation = angle;
+            RefreshMatrix();
+        }
+
+        public void SetScale(float value)
+        {
+            if (Math.Abs(value - Scale) < float.Epsilon)
+                return;
+
+            Scale = value;
+            RefreshMatrix();
+        }
+
+        private void RefreshMatrix()
+        {
+            Matrix = new Matrix3X3(Position, Rotation, Scale);
+        }
+    }
+}
