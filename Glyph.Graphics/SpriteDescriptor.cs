3749cd4965efd91436b71bc191ab535e9c1883ed
ReMinoer <rmorillon@freneticexperience.com>
2015-07-08 22:57:59 +0200

    Add SpriteDescriptor component

---
+++ Glyph.Graphics/SpriteDescriptor.cs
@@ -0,0 +1,89 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Glyph.Graphics
+{
+    public class SpriteDescriptor : GlyphComponent
+    {
+        private enum AnchorType
+        {
+            Origin,
+            Pivot
+        }
+
+        private Texture2D _texture;
+        private Vector2 _origin;
+        private Vector2 _pivot;
+        private AnchorType _lastAnchorType;
+
+        public Rectangle? SourceRectangle { get; set; }
+        public Color Color { get; set; }
+        public SpriteEffects Effects { get; set; }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+            set
+            {
+                _texture = value;
+
+                if (Texture == null)
+                    SourceRectangle = null;
+                else if (SourceRectangle == null)
+                    SourceRectangle = Texture.Bounds;
+
+                RefreshAnchor();
+            }
+        }
+
+        public Vector2 Origin
+        {
+            get { return _origin; }
+            set
+            {
+                _origin = value;
+
+                _lastAnchorType = AnchorType.Origin;
+                RefreshAnchor();
+            }
+        }
+
+        public Vector2 Pivot
+        {
+            get { return _pivot; }
+            set
+            {
+                _pivot = value;
+
+                _lastAnchorType = AnchorType.Pivot;
+                RefreshAnchor();
+            }
+        }
+
+        public SpriteDescriptor()
+        {
+            Color = Color.White;
+            Pivot = Vector2.One * 0.5f;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            Texture.Dispose();
+        }
+
+        private void RefreshAnchor()
+        {
+            switch (_lastAnchorType)
+            {
+                case AnchorType.Origin:
+                    _pivot = Texture.Size() / _origin;
+                    break;
+                case AnchorType.Pivot:
+                    _origin = Texture.Size() * _pivot;
+                    break;
+            }
+        }
+    }
+}
--- Glyph.Graphics/SpriteRenderer.cs
+++ Glyph.Graphics/SpriteRenderer.cs
@@ -1,27 +1,18 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 
 namespace Glyph.Graphics
 {
-    public interface ISpriteDescriptor
-    {
-        Texture2D Texture { get; }
-        Rectangle? SourceRectangle { get; }
-        Color Color { get; }
-        Vector2 Origin { get; }
-        SpriteEffects Effects { get; }
-    }
-
     public class SpriteRenderer : GlyphComponent, IDraw
     {
         private readonly SceneNode _sceneNode;
+        private readonly SpriteDescriptor _sprite;
         private readonly SpriteBatch _spriteBatch;
         public bool Visible { get; set; }
-        public ISpriteDescriptor Sprite { get; set; }
 
-        public SpriteRenderer(SceneNode sceneNode, SpriteBatch spriteBatch)
+        public SpriteRenderer(SceneNode sceneNode, SpriteDescriptor sprite, SpriteBatch spriteBatch)
         {
             _sceneNode = sceneNode;
+            _sprite = sprite;
             _spriteBatch = spriteBatch;
         }
 
@@ -30,8 +21,8 @@ namespace Glyph.Graphics
             if (!Visible)
                 return;
 
-            _spriteBatch.Draw(Sprite.Texture, _sceneNode.Position, Sprite.SourceRectangle, Sprite.Color,
-                _sceneNode.Rotation, Sprite.Origin, _sceneNode.Scale, Sprite.Effects, 0);
+            _spriteBatch.Draw(_sprite.Texture, _sceneNode.Position, _sprite.SourceRectangle, _sprite.Color,
+                _sceneNode.Rotation, _sprite.Origin, _sceneNode.Scale, _sprite.Effects, 0);
         }
     }
 }
