2190f2471293699c9b3b4c62ddef258a97efd9e6
ReMinoer <rmorillon@freneticexperience.com>
2015-10-25 23:28:49 +0100

    Add float extension to handle zero equality

---
+++ Glyph/Float.Extension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glyph
+{
+    static public class FloatExtension
+    {
+        static public bool EqualsZero(this float value)
+        {
+            return Math.Abs(value) < float.Epsilon;
+        }
+    }
+}
