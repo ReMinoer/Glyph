726ca1f353249625b0348749de2398b9e308d0d6
ReMinoer <rmorillon@freneticexperience.com>
2015-11-08 21:27:08 +0100

    Handle PhysicsManager lenght unit

--- Glyph.Physics/PhysicsManager.cs
+++ Glyph.Physics/PhysicsManager.cs
@@ -4,10 +4,12 @@
     {
         public const float EarthGravity = 9.80665f;
         public float Gravity { get; set; }
+        public float LengthUnit { get; set; }
 
         public PhysicsManager()
         {
             Gravity = EarthGravity;
+            LengthUnit = 1f;
         }
     }
 }
