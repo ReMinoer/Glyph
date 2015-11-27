8b7266e23d0e4aba4857d0b37eb4ab13f8cbb6f0
ReMinoer <rmorillon@freneticexperience.com>
2015-11-27 15:10:15 +0100

    Add FillingRectangle for FillingRenderer usage

---
+++ Glyph.Graphics/FillingRectangle.cs
@@ -0,0 +1,10 @@
+﻿using Glyph.Composition;
+using Glyph.Math.Shapes;
+
+namespace Glyph.Graphics
+{
+    public class FillingRectangle : GlyphComponent
+    {
+         public IRectangle Rectangle { get; set; }
+    }
+}
--- Glyph.Graphics/FillingRenderer.cs
+++ Glyph.Graphics/FillingRenderer.cs
@@ -1,6 +1,5 @@
 ﻿using Glyph.Animation;
 using Glyph.Math;
-using Glyph.Math.Shapes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -8,21 +7,21 @@ namespace Glyph.Graphics
 {
     public class FillingRenderer : Renderer
     {
-        private readonly IRectangle _rectangle;
+        private readonly FillingRectangle _fillingRectangle;
 
-        public FillingRenderer(IRectangle rectangle, ISpriteSource source, SceneNode sceneNode)
+        public FillingRenderer(FillingRectangle fillingRectangle, ISpriteSource source, SceneNode sceneNode)
             : base(source, sceneNode)
         {
-            _rectangle = rectangle;
+            _fillingRectangle = fillingRectangle;
         }
 
         protected override void Render(SpriteBatch spriteBatch)
         {
             if (SpriteTransformer != null)
-                spriteBatch.Draw(Source.Texture, _rectangle.ToStruct(), Source.Rectangle, SpriteTransformer.Color,
+                spriteBatch.Draw(Source.Texture, _fillingRectangle.Rectangle.ToStruct(), Source.Rectangle, SpriteTransformer.Color,
                     SceneNode.Rotation, SpriteTransformer.Origin, SpriteTransformer.Effects, 0);
             else
-                spriteBatch.Draw(Source.Texture, _rectangle.ToStruct(), Source.Rectangle, Color.White,
+                spriteBatch.Draw(Source.Texture, _fillingRectangle.Rectangle.ToStruct(), Source.Rectangle, Color.White,
                     SceneNode.Rotation, Vector2.Zero, SpriteEffects.None, 0);
         }
     }
