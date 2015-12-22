f061d27d062b2d89efda7060aa5fd6102bad311c
ReMinoer <rmorillon@freneticexperience.com>
2015-12-22 18:16:45 +0100

    Add GlyphData

---
+++ Glyph/GlyphData.cs
@@ -0,0 +1,20 @@
+﻿using Diese.Injection;
+using Diese.Modelization;
+
+namespace Glyph
+{
+    public abstract class GlyphData<T> : ICreationData<T>, IConfigurationData<T>
+    {
+        public IDependencyInjector Injector { protected get; set; }
+
+        public abstract void From(T obj);
+        public abstract void Configure(T obj);
+
+        public T Create()
+        {
+            var obj = Injector.Resolve<T>();
+            Configure(obj);
+            return obj;
+        }
+    }
+}
