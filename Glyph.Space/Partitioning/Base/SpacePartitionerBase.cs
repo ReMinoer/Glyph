c15966b8c09b227f885ff50422e998b63062c2fd
ReMinoer <rmorillon@freneticexperience.com>
2016-01-24 22:01:52 +0100

    Add Glyph.Space, base for partitioning & implements QuadtreeSpacePartitioner

---
+++ Glyph.Space/Partitioning/Base/SpacePartitionerBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Glyph.Math;
+using Microsoft.Xna.Framework;
+
+namespace Glyph.Space.Partitioning.Base
+{
+    public abstract class SpacePartitionerBase<T> : ISpacePartitioner<T>
+    {
+        protected Func<T, Vector2> GetPosition;
+        protected abstract PartitionBase<T> Root { get; }
+        public SpacePartitionerParameters Parameters { get; private set; }
+
+        protected SpacePartitionerBase(Func<T, Vector2> getPosition, SpacePartitionerParameters parameters)
+        {
+            GetPosition = getPosition;
+            Parameters = parameters;
+        }
+
+        public bool ContainsPoint(Vector2 point)
+        {
+            return Root.ContainsPoint(point);
+        }
+
+        public bool Insert(T item)
+        {
+            return Root.TryInsert(item, 0, Parameters.DepthMax);
+        }
+
+        public bool Remove(T item)
+        {
+            return Root.TryRemove(item);
+        }
+
+        public IPartition<T> GetPartition(Vector2 position)
+        {
+            return Root.GetBestPartition(position);
+        }
+
+        public IEnumerable<T> GetAllPointsInRange(IShape range)
+        {
+            return Root.GetAllPointsInRange(range);
+        }
+    }
+}
---
+++ Glyph.Space/Partitioning/ISpacePartitionerParameters.cs
@@ -0,0 +1,18 @@
+﻿using Glyph.Math.Shapes;
+
+namespace Glyph.Space.Partitioning
+{
+    public class SpacePartitionerParameters
+    {
+        public int NodeCapacity { get; private set; }
+        public int DepthMax { get; private set; }
+        public IRectangle Bounds { get; private set; }
+
+        public SpacePartitionerParameters(int nodeCapacity, int depthMax, IRectangle bounds)
+        {
+            NodeCapacity = nodeCapacity;
+            DepthMax = depthMax;
+            Bounds = bounds;
+        }
+    }
+}
