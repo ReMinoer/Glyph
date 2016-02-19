025ff77e4653d0bd928dc326bab68747d13c8892
ReMinoer <rmorillon@freneticexperience.com>
2016-02-20 00:40:49 +0100

    Add IShapedObject

---
+++ Glyph.Composition/IShapedObject.cs
@@ -0,0 +1,16 @@
+﻿using Glyph.Math;
+
+namespace Glyph.Composition
+{
+    public interface IShapedObject : IGlyphComponent
+    {
+        ISceneNode SceneNode { get; }
+        IShape Shape { get; }
+    }
+
+    public interface IShapedObject<out T> : IShapedObject
+        where T : IShape
+    {
+        new T Shape { get; }
+    }
+}
