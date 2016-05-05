6c6cc7fe833bd39ecbe06c0306ec3775eeff2b8c
Rémi Morillon <rmorillon@freneticexperience.com>
2016-05-05 20:45:18 +0200

    Add margin to UniformCarver

--- Glyph.Graphics/Carvers/UniformCarver.cs
+++ Glyph.Graphics/Carvers/UniformCarver.cs
@@ -7,30 +7,32 @@ namespace Glyph.Graphics.Carvers
     {
         public int Columns { get; set; }
         public int Rows { get; set; }
+        public int Margin { get; set; }
 
-        public UniformCarver(int columns, int rows)
+        public UniformCarver(int columns, int rows, int margin = 0)
         {
             if (columns < 1)
-                throw new ArgumentOutOfRangeException("columns");
+                throw new ArgumentOutOfRangeException(nameof(columns));
             if (rows < 1)
-                throw new ArgumentOutOfRangeException("rows");
+                throw new ArgumentOutOfRangeException(nameof(rows));
 
             Columns = columns;
             Rows = rows;
+            Margin = margin;
         }
 
         public void Process(SpriteSheet spriteSheet)
         {
-            int width = spriteSheet.Texture.Width / Columns;
-            int height = spriteSheet.Texture.Height / Rows;
+            int width = spriteSheet.Texture.Width / Columns - 2 * Margin;
+            int height = spriteSheet.Texture.Height / Rows - 2 * Margin;
 
             for (int r = 0; r < Rows; r++)
                 for (int c = 0; c < Columns; c++)
                 {
                     var frame = new Rectangle
                     {
-                        X = c * width,
-                        Y = r * height,
+                        X = c * spriteSheet.Texture.Width / Columns + Margin,
+                        Y = r * spriteSheet.Texture.Height / Rows + Margin,
                         Width = width,
                         Height = height
                     };
