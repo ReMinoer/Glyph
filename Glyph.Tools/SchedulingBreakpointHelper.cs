99163e20b8af557e71828115b1f99afe5bb0b628
Rémi Morillon <rmorillon@freneticexperience.com>
2016-05-22 00:14:27 +0200

    Implement SchedulingBreakpointHelper

---
+++ Glyph.Tools/SchedulingBreakpointHelper.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+
+namespace Glyph.Tools
+{
+    static public class SchedulingBreakpointHelper
+    {
+        static private readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static public void Initialize()
+        {
+            Logger.Debug("Initialize breakpoint");
+        }
+
+        static public void LoadContent(ContentLibrary contentLibrary)
+        {
+            Logger.Debug("LoadContent breakpoint");
+        }
+
+        static public void Update(ElapsedTime elapsedTime)
+        {
+            Logger.Debug("Update breakpoint");
+        }
+
+        static public void Draw(IDrawer drawer)
+        {
+            Logger.Debug("Draw breakpoint");
+        }
+    }
+}
