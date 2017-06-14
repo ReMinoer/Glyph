3fe55a8d22d51915c3011fbb96803224830fe650
Rémi Morillon <rmorillon@freneticexperience.com>
2017-06-14 17:30:02 +0200

    Add Thickness to CircleSprite

--- Glyph.Graphics/Shapes/CircleSprite.cs
+++ Glyph.Graphics/Shapes/CircleSprite.cs
@@ -6,12 +6,12 @@ namespace Glyph.Graphics.Shapes
 {
     public class CircleSprite : ShapedSpriteBase
     {
-        public int Radius { get; set; }
+        public int Radius { get; set; } = 50;
+        public int Thickness { get; set; } = 1;
 
         public CircleSprite(Func<GraphicsDevice> graphicsDeviceFunc)
             : base(graphicsDeviceFunc)
         {
-            Radius = 50;
         }
 
         protected override void GenerateTexture()
@@ -23,16 +23,19 @@ namespace Glyph.Graphics.Shapes
             for (int i = 0; i < data.Length; i++)
                 data[i] = Color.Transparent;
 
-            // Work out the minimum step necessary using trigonometry + sine approximation.
-            double angleStep = 1f / Radius;
-
-            for (double angle = 0; angle < System.Math.PI * 2; angle += angleStep)
+            for (int i = 0; i < Thickness; i++)
             {
-                // Use the parametric definition of a circle: http://en.wikipedia.org/wiki/Circle#Cartesian_coordinates
-                int x = (int)System.Math.Round(Radius + Radius * System.Math.Cos(angle));
-                int y = (int)System.Math.Round(Radius + Radius * System.Math.Sin(angle));
+                // Work out the minimum step necessary using trigonometry + sine approximation.
+                double angleStep = 1f / (Radius - i);
 
-                data[y * outerDiameter + x + 1] = Color;
+                for (double angle = 0; angle < System.Math.PI * 2; angle += angleStep)
+                {
+                    // Use the parametric definition of a circle: http://en.wikipedia.org/wiki/Circle#Cartesian_coordinates
+                    int x = (int)System.Math.Round(Radius + (Radius - i) * System.Math.Cos(angle));
+                    int y = (int)System.Math.Round(Radius + (Radius - i) * System.Math.Sin(angle));
+
+                    data[y * outerDiameter + x + 1] = Color;
+                }
             }
 
             _texture = new Texture2D(GraphicsDeviceFunc(), outerDiameter, outerDiameter);
