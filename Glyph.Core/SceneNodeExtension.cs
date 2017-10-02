d307a8cfa129e13893b20caeed26682db4bdb9f2
Rémi Morillon <rmorillon@freneticexperience.com>
2017-10-02 17:33:58 +0200

    Add ISceneNode extension DistanceTo

---
+++ Glyph.Core/SceneNodeExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Glyph.Core
+{
+    static public class SceneNodeExtension
+    {
+        static public float DistanceTo(this ISceneNode a, ISceneNode b)
+        {
+            return (a.Position - b.Position).Length();
+        }
+    }
+}
