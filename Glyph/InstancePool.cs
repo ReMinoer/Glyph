6336d7c606e1bcb4b1c0ace69f3e57163a32a0a4
Rémi Morillon <rmorillon@freneticexperience.com>
2017-10-21 16:52:58 +0200

    Add InstancePool

---
+++ Glyph/InstancePool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Glyph
+{
+    internal class InstancePool<T>
+    {
+        private readonly Func<T> _factory;
+        private readonly ConcurrentBag<T> _concurrentBag = new ConcurrentBag<T>();
+
+        public InstancePool(Func<T> factory)
+        {
+            _factory = factory;
+        }
+
+        public T Get()
+        {
+            if (!_concurrentBag.TryTake(out T item))
+                item = _factory();
+            return item;
+        }
+
+        public void Recondition(T usedSubject)
+        {
+            _concurrentBag.Add(usedSubject);
+        }
+    }
+}
