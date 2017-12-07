ed82a7eb841a8fee02a5e81a9b1bce4e5d03072c
Rémi Morillon <rmorillon@freneticexperience.com>
2017-12-07 21:13:48 +0100

    Add GlyphCreator

---
+++ Glyph.Composition/Modelization/GlyphCreatorBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+using Diese.Injection;
+
+namespace Glyph.Composition.Modelization
+{
+    public abstract class GlyphCreatorBase<T> : IGlyphCreator<T>
+    {
+        [XmlIgnore]
+        public IDependencyInjector Injector { protected get; set; }
+        public abstract T Create();
+    }
+}
---
+++ Glyph.Composition/Modelization/IGlyphCreator.cs
@@ -0,0 +1,10 @@
+﻿using Diese.Injection;
+using Diese.Modelization;
+
+namespace Glyph.Composition.Modelization
+{
+    public interface IGlyphCreator<out T> : ICreator<T>
+    {
+        IDependencyInjector Injector { set; }
+    }
+}
