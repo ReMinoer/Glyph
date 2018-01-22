67e23512d39e7ae843005a04b860a6808286ac12
Rémi Morillon <rmorillon@freneticexperience.com>
2018-01-22 21:28:58 +0100

    Refactor cursor controls

---
+++ Glyph.Core/Inputs/CursorControlBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Fingear;
+using Fingear.Controls.Base;
+
+namespace Glyph.Core.Inputs
+{
+    public abstract class CursorControlBase : ControlBase<System.Numerics.Vector2>
+    {
+        private System.Numerics.Vector2? _lastValue;
+        public ICursorInput Input { get; set; }
+
+        public override sealed IEnumerable<IInput> Inputs
+        {
+            get { yield return Input; }
+        }
+
+        protected CursorControlBase(string name, ICursorInput input)
+        {
+            Name = name;
+            Input = input;
+        }
+
+        protected override sealed bool UpdateControl(float elapsedTime, out System.Numerics.Vector2 value)
+        {
+            if (Input == null || !ComputeCursor(out value))
+            {
+                value = default;
+                _lastValue = value;
+                return false;
+            }
+
+            bool isTriggered = value != _lastValue;
+            _lastValue = value;
+            return isTriggered;
+        }
+
+        protected abstract bool ComputeCursor(out System.Numerics.Vector2 value);
+    }
+}
