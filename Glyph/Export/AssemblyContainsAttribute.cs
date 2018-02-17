b8168d0886f65a87a2d7373166696b091e8ce384
Rémi Morillon <rmorillon@freneticexperience.com>
2018-02-17 10:20:34 +0100

    Introduce type export/import classes

---
+++ Glyph/Export/AssemblyContainsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Glyph.Export
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class AssemblyContainsAttribute : Attribute
+    {
+        public Type Type { get; }
+
+        public AssemblyContainsAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ Glyph/Export/TypeImporter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Glyph.Export
+{
+    static public class TypeImporter
+    {
+        static public List<Type> GetTypesFromAssemblies(IEnumerable<Assembly> assemblies)
+        {
+            var importedTypes = new List<Type>();
+            foreach (Assembly assembly in assemblies)
+                foreach (AssemblyContainsAttribute attribute in assembly.GetCustomAttributes<AssemblyContainsAttribute>())
+                    importedTypes.AddRange(assembly.GetExportedTypes().Where(t => attribute.Type.IsAssignableFrom(t)));
+            
+            return importedTypes;
+        }
+
+        static public List<Type> GetTypesFromLocalAssemblies()
+        {
+            string assemblyLocation = Assembly.GetAssembly(typeof(TypeImporter)).Location;
+            string assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+            if (assemblyDirectory == null)
+                throw new InvalidOperationException();
+
+            Assembly CurrentDomainOnReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs e) => Assembly.ReflectionOnlyLoad(e.Name);
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += CurrentDomainOnReflectionOnlyAssemblyResolve;
+
+            IEnumerable<Assembly> assemblies = Directory.GetFiles(assemblyDirectory, "*.dll", SearchOption.TopDirectoryOnly)
+                                                        .Select(Assembly.ReflectionOnlyLoadFrom)
+                                                        .Where(a => a.CustomAttributes.Any(x => x.AttributeType.Name == typeof(AssemblyContainsAttribute).Name))
+                                                        .ToArray();
+
+            AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve -= CurrentDomainOnReflectionOnlyAssemblyResolve;
+            
+            return GetTypesFromAssemblies(assemblies.Select(a => Assembly.Load(a.GetName())));
+        }
+    }
+}
