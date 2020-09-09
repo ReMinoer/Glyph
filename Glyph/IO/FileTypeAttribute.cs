8fee5f7942fa9a3dc01e53719557e26140dad3ff
ReMinoer <rmorillon@freneticexperience.com>
2020-09-09 21:31:42 +0200

    Add path attributes for file type and root folder

---
+++ Glyph/IO/FileTypeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Glyph.IO
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public class FileTypeAttribute : Attribute
+    {
+        public FileType FileType { get; private set; }
+
+        public string DisplayName
+        {
+            get => FileType.DisplayName;
+            set => FileType = new FileType
+            {
+                DisplayName = value,
+                Extensions = FileType.Extensions
+            };
+        }
+
+        public override object TypeId => FileType;
+
+        public FileTypeAttribute(params string[] extensions)
+        {
+            FileType = new FileType
+            {
+                DisplayName = extensions?.FirstOrDefault()?.TrimStart('.').ToUpper() + " Files",
+                Extensions = extensions
+            };
+        }
+    }
+}
---
+++ Glyph/IO/RootFolderAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Glyph.IO
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class RootFolderAttribute : Attribute
+    {
+        public string Path { get; }
+
+        public RootFolderAttribute(string path)
+        {
+            Path = path;
+        }
+
+        public RootFolderAttribute(Environment.SpecialFolder specialFolder)
+        {
+            Path = Environment.GetFolderPath(specialFolder);
+        }
+    }
+}
