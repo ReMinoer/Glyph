2374e4733cd044724db5a6789b8dace51c16648f
ReMinoer <reminoer@gmail.com>
2020-10-04 10:28:00 +0200

    Improve FileType

--- Glyph/IO/FileType.cs
+++ Glyph/IO/FileType.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Glyph.IO
 {
     public struct FileType
     {
         public string DisplayName { get; set; }
-        public IEnumerable<string> Extensions { get; set; }
+        public string[] Extensions { get; set; }
 
         public FileType(params string[] extensions)
         {
             DisplayName = extensions?.First().TrimStart('.').ToUpper() + " Files";
             Extensions = extensions;
         }
+
+        static public FileType All => new FileType(".*") { DisplayName = "All Files" };
     }
 }
