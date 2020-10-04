0bd072c3c658bc0b96208f0e7831a8b2b7d6dba5
ReMinoer <reminoer@gmail.com>
2020-10-04 10:28:12 +0200

    Add FileContent attribute

---
+++ Glyph/IO/FileContentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Glyph.IO
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class FileContentAttribute : Attribute
+    {
+        public Type ContentType { get; }
+
+        public FileContentAttribute(Type contentType)
+        {
+            ContentType = contentType;
+        }
+    }
+}
