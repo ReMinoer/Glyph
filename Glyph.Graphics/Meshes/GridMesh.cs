03e82e950e4cb10b52a153e69116e902841ac8da
ReMinoer <reminoer@gmail.com>
2020-11-21 11:56:10 +0100

    Add meshing strategy to grid meshes

--- Glyph.Graphics/Meshes/GridMesh.cs
+++ Glyph.Graphics/Meshes/GridMesh.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using Glyph.Graphics.Meshes.Base;
-using Glyph.Graphics.Meshes.Utils;
 using Glyph.Space;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Simulacra.Utils;
 
 namespace Glyph.Graphics.Meshes
 {
-    public class GridMesh<T> : ProceduralMeshBase
+    public class GridMesh<TGrid, T, TInfo> : ProceduralMeshBase
+        where TGrid : class, IDirtableGrid<T>
         where T : class, IDirtable
     {
-        private IDirtableGrid<T> _grid;
-        public IDirtableGrid<T> Grid
+        private TGrid _grid;
+        public TGrid Grid
         {
             get => _grid;
             set
@@ -33,8 +34,8 @@ namespace Glyph.Graphics.Meshes
             }
         }
 
-        private Func<int, int, T, bool> _meshingBehavior;
-        public Func<int, int, T, bool> MeshingBehavior
+        private IGridMeshing<TGrid, T, TInfo> _meshingBehavior;
+        public IGridMeshing<TGrid, T, TInfo> MeshingBehavior
         {
             get => _meshingBehavior;
             set
@@ -42,8 +43,16 @@ namespace Glyph.Graphics.Meshes
                 if (_meshingBehavior == value)
                     return;
 
+                if (_meshingBehavior != null)
+                    _meshingBehavior.Changed -= OnMeshingChanged;
+
                 _meshingBehavior = value;
                 DirtyCaches();
+
+                if (_meshingBehavior != null)
+                    _meshingBehavior.Changed += OnMeshingChanged;
+
+                void OnMeshingChanged(object sender, EventArgs e) => DirtyCaches();
             }
         }
 
@@ -52,36 +61,98 @@ namespace Glyph.Graphics.Meshes
 
         protected override void RefreshCache(List<Vector2> vertices, List<int> indices)
         {
+            // TODO: Use sorted list
             vertices.Clear();
             indices.Clear();
 
-            if (Grid != null)
-            {
-                List<Rectangle> rectangles = MeshHelpers.GetRectanglesFromArray(Grid, MeshingBehavior);
-                foreach (Rectangle rectangle in rectangles)
-                {
-                    AddVertex(vertices, indices, rectangle.Top, rectangle.Left);
-                    AddVertex(vertices, indices, rectangle.Top, rectangle.Right);
-                    AddVertex(vertices, indices, rectangle.Bottom, rectangle.Left);
+            if (Grid == null || MeshingBehavior == null)
+                return;
 
-                    AddVertex(vertices, indices, rectangle.Top, rectangle.Right);
-                    AddVertex(vertices, indices, rectangle.Bottom, rectangle.Right);
-                    AddVertex(vertices, indices, rectangle.Bottom, rectangle.Left);
+            // Create byte mask
+            var mask = new TInfo[Grid.GetLength(0), Grid.GetLength(1)];
+            mask.GetResetIndex(out int i, out int j);
+            while (mask.MoveIndex(ref i, ref j))
+            {
+                mask[i, j] = MeshingBehavior.GetCellInfo(Grid, i, j);
+            }
+
+            // Process mask
+            mask.GetResetIndex(out i, out j);
+            if (MeshingBehavior.CanContainsRectangles)
+            {
+                while (mask.MoveIndex(ref i, ref j))
+                {
+                    TInfo cellInfo = mask[i, j];
+
+                    if (MeshingBehavior.IsPartOfRectangle(cellInfo))
+                    {
+                        MeshingBehavior.AddRectangle(vertices, indices, Grid, GetRectangle(mask, i, j));
+                    }
+                    else if (MeshingBehavior.IsCell(cellInfo))
+                    {
+                        MeshingBehavior.AddCell(vertices, indices, Grid, i, j, cellInfo);
+                    }
+                }
+            }
+            else
+            {
+                while (mask.MoveIndex(ref i, ref j))
+                {
+                    TInfo cellInfo = mask[i, j];
+
+                    if (MeshingBehavior.IsCell(mask[i, j]))
+                    {
+                        MeshingBehavior.AddCell(vertices, indices, Grid, i, j, cellInfo);
+                    }
                 }
             }
         }
 
-        private void AddVertex(List<Vector2> vertices, List<int> indices, int i, int j)
+        private Rectangle GetRectangle(TInfo[,] mask, int i, int j)
         {
-            Vector2 vertex = Grid.ToWorldPoint(i, j);
-            int index = vertices.IndexOf(vertex);
-            if (index == -1)
+            // Get width on first row
+            int width = 1;
+            int jj = j;
+            if (mask.MoveIndex(ref jj, dimension: 1))
             {
-                index = vertices.Count;
-                vertices.Add(vertex);
+                while (MeshingBehavior.IsPartOfRectangle(mask[i, jj]))
+                {
+                    mask[i, jj] = MeshingBehavior.EmptyInfo;
+                    width++;
+                    if (!mask.MoveIndex(ref jj, dimension: 1))
+                        break;
+                }
             }
 
-            indices.Add(index);
+            // Add to height all rows matching width
+            int height = 1;
+            int ii = i;
+            while (mask.MoveIndex(ref ii, dimension: 0))
+            {
+                bool matchingFirstRow = true;
+                for (jj = j; jj < j + width; jj++)
+                {
+                    if (!MeshingBehavior.IsPartOfRectangle(mask[ii, jj]))
+                    {
+                        matchingFirstRow = false;
+                        break;
+                    }
+                }
+
+                // If current row cannot contains chosen width, fix mask and stop here
+                if (!matchingFirstRow)
+                    break;
+
+                // Else, increment height and continue with next row
+                height++;
+
+                for (jj = j; jj < j + width; jj++)
+                {
+                    mask[ii, jj] = MeshingBehavior.EmptyInfo;
+                }
+            }
+
+            return new Rectangle(j, i, width, height);
         }
     }
 }
---
+++ Glyph.Graphics/Meshes/IGridMeshing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Glyph.Space;
+using Microsoft.Xna.Framework;
+
+namespace Glyph.Graphics.Meshes
+{
+    public interface IGridMeshing<in TGrid, in T, TInfo>
+        where TGrid : IGrid<T>
+    {
+        event EventHandler Changed;
+        TInfo EmptyInfo { get; }
+        TInfo GetCellInfo(TGrid grid, int i, int j);
+
+        bool CanContainsRectangles { get; }
+        bool IsPartOfRectangle(TInfo cellInfo);
+        void AddRectangle(List<Vector2> vertices, List<int> indices, TGrid grid, Rectangle rectangle);
+
+        bool IsCell(TInfo cellInfo);
+        void AddCell(List<Vector2> vertices, List<int> indices, TGrid grid, int i, int j, TInfo info);
+    }
+}
