69153c549a53462f3546a67bb43a54f7c036683c
ReMinoer <reminoer@gmail.com>
2021-01-10 13:41:55 +0100

    Add NullDisposable

---
+++ Glyph/Utils/NullDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glyph.Utils
+{
+    public class NullDisposable : IDisposable
+    {
+        static private NullDisposable _instance;
+        static public NullDisposable Instance => _instance ?? (_instance = new NullDisposable());
+
+        public void Dispose() { }
+    }
+}
