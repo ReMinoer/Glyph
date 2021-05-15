b232c4c54798f5243703a223051d43fba65244af
ReMinoer <reminoer@gmail.com>
2021-05-15 17:37:57 +0200

    Fix missing interface implementation for IRawContentLibrary

--- Glyph.Pipeline/IRawContentLibrary.cs
+++ Glyph.Pipeline/IRawContentLibrary.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Glyph.Pipeline
 {
-    public interface IRawContentLibrary
+    public interface IRawContentLibrary : IContentLibrary
     {
         ICollection<string> GetRawFilesPaths(string assetPath);
         IEnumerable<string> GetSupportedFileExtensions(Type type);
